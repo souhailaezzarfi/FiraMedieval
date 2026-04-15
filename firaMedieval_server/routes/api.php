52e7ab7c6612ab84fa1af1657b268fadc5468672
Àlex Masegosa <alex_masegosanavio@iescarlesvallbona.cat>
2026-04-15 17:22:46 +0200

    feat: add api routes

--- firaMedieval_server/routes/api.php
+++ firaMedieval_server/routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ActivitatController;
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\AparcamentController;
+use App\Http\Controllers\Api\ReservaAutocaravanaController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Activitats
+Route::apiResource('activitats', ActivitatController::class);
+Route::apiResource('categories', CategoriaController::class);
+Route::apiResource('aparcaments', AparcamentController::class);
+
+// Reserva autocaravanes
+Route::apiResource('reserves-autocaravanes', ReservaAutocaravanaController::class);
