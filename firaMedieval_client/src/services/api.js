26f35bdcb246d4d2cabbd2e73f200ddb033c7f9c
Àlex Masegosa <alex_masegosanavio@iescarlesvallbona.cat>
2026-05-07 17:31:38 +0200

    fix: solve api cors issues

--- firaMedieval_client/src/services/api.js
+++ firaMedieval_client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.FRONTEND_URL || "http://localhost:8000/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
